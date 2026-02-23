cc299ba844b5103e61df3bea8ed73510cc2c80c0
batjargal1 <batjargal@omrt.tech>
2026-02-24 00:48:21 +0100

    test

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 db = SQLAlchemy(app)
